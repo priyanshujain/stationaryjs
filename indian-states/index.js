2a5755aa9123c8b31eb16d1fda7d2a0008373604
Priyanshu Jain <priyanshu@launchyard.com>
2018-02-10 22:42:50 +0530

    add indian states

---
+++ indian-states/index.js
@@ -0,0 +1,148 @@
+const indianStates = [
+  {
+    value: "Andaman and Nicobar Islands",
+    name: "Andaman and Nicobar Islands"
+  },
+  {
+    value: "Andhra Pradesh",
+    name: "Andhra Pradesh"
+  },
+  {
+    value: "Arunachal Pradesh",
+    name: "Arunachal Pradesh"
+  },
+  {
+    value: "Assam",
+    name: "Assam"
+  },
+  {
+    value: "Bihar",
+    name: "Bihar"
+  },
+  {
+    value: "Chhattisgarh",
+    name: "Chhattisgarh"
+  },
+  {
+    value: "Chandigarh",
+    name: "Chandigarh"
+  },
+  {
+    value: "Dadra and Nagar Haveli",
+    name: "Dadra and Nagar Haveli"
+  },
+  {
+    value: "Daman and Diu",
+    name: "Daman and Diu"
+  },
+  {
+    value: "Delhi",
+    name: "Delhi"
+  },
+  {
+    value: "Goa",
+    name: "Goa"
+  },
+  {
+    value: "Gujarat",
+    name: "Gujarat"
+  },
+  {
+    value: "Haryana",
+    name: "Haryana"
+  },
+  {
+    value: "Himachal Pradesh",
+    name: "Himachal Pradesh"
+  },
+  {
+    value: "Jammu and Kashmir",
+    name: "Jammu and Kashmir"
+  },
+  {
+    value: "Jharkhand",
+    name: "Jharkhand"
+  },
+  {
+    value: "Karnataka",
+    name: "Karnataka"
+  },
+  {
+    value: "Kerala",
+    name: "Kerala"
+  },
+  {
+    value: "Lakshadweep",
+    name: "Lakshadweep"
+  },
+  {
+    value: "Madhya Pradesh",
+    name: "Madhya Pradesh"
+  },
+  {
+    value: "Maharashtra",
+    name: "Maharashtra"
+  },
+  {
+    value: "Manipur",
+    name: "Manipur"
+  },
+  {
+    value: "Meghalaya",
+    name: "Meghalaya"
+  },
+  {
+    value: "Mizoram",
+    name: "Mizoram"
+  },
+  {
+    value: "Nagaland",
+    name: "Nagaland"
+  },
+  {
+    value: "Orissa",
+    name: "Orissa"
+  },
+  {
+    value: "Punjab",
+    name: "Punjab"
+  },
+  {
+    value: "Puducherry",
+    name: "Puducherry"
+  },
+  {
+    value: "Rajasthan",
+    name: "Rajasthan"
+  },
+  {
+    value: "Sikkim",
+    name: "Sikkim"
+  },
+  {
+    value: "Tamil Nadu",
+    name: "Tamil Nadu"
+  },
+  {
+    value: "Telangana",
+    name: "Telangana"
+  },
+  {
+    value: "Tripura",
+    name: "Tripura"
+  },
+  {
+    value: "Uttar Pradesh",
+    name: "Uttar Pradesh"
+  },
+  {
+    value: "Uttarakhand",
+    name: "Uttarakhand"
+  },
+  {
+    value: "West Bengal",
+    name: "West Bengal"
+  }
+];
+
+export default indianStates;
